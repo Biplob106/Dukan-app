afa94ea58f7914f53f589d69983b48986e9a3523
Biplob106 <biplobsd106@gmail.com>
2025-10-05 17:25:12 +0600

    add logout

--- routes.php
+++ routes.php
@@ -6,3 +6,4 @@ $router->get('/register','registration/create.php');
 $router->post('/register','registration/store.php');
 $router->get('/login','sessions/create.php');
 $router->post('/sessions','sessions/store.php');
+$router->delete('/sessions','sessions/destroy.php')->only('auth');
